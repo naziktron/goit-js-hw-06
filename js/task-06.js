fe76a99f9a2c9ac6e68b9d9414d6f37828ab7566
Nazariy <44504451+naziktron@users.noreply.github.com>
2022-04-28 17:06:51 +0300

    done

--- js/task-06.js
+++ js/task-06.js
@@ -7,6 +7,7 @@ inputEl.addEventListener('blur', onBlur);
 
 function onBlur(event){
     this.classList.add('invalid');
+    this.classList.remove('valid');
     if(Number(event.currentTarget.value.length)===Number(event.currentTarget.dataset.length)){
         this.classList.add('valid');
         this.classList.remove('invalid');
